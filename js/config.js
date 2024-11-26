e2a304c29c2e6af4ea7b7e651a29a017dfa4b654
Rog3rc <ortizriosvictorguillermo@gmail.com>
2024-11-26 00:23:28 -0600

    msg: configurable client-id

--- js/config.js
+++ js/config.js
@@ -1,6 +1,4 @@
 export const CONFIG = {
   URL: "https://obs-try-widget.vercel.app",
-  //   URL: "http://localhost:5500",
-  //   CLIENT_ID: "0f6d3043ba1242ea96c1e8547827af7b",
-  CLIENT_ID: "dad532ec13d84e508c6c8412c441a394",
+  // URL: "http://localhost:5500",
 };
--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 import { CONFIG } from "./config.js";
 
-const clientId = CONFIG.CLIENT_ID;
+let clientId;
 const redirectUri = CONFIG.URL; // Debe coincidir con el URI registrado en Spotify
 const scopes = [
   "user-read-playback-state",
@@ -9,9 +9,11 @@ const scopes = [
 ];
 
 document.getElementById("login").addEventListener("click", () => {
+  clientId = document.getElementById("textId").value;
   const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&redirect_uri=${encodeURIComponent(
     redirectUri
   )}&scope=${encodeURIComponent(scopes.join(" "))}`;
+  document.getElementById("textId").value = "";
   window.location.href = authUrl;
 });
 
@@ -30,6 +32,7 @@ window.onload = async () => {
   accessToken = getTokenFromUrl();
   if (accessToken) {
     document.getElementById("login").style.display = "none";
+    document.getElementById("textId").style.display = "none";
     document.getElementById("player").style.display = "block";
     await getCurrentTrack();
     startTimer();
