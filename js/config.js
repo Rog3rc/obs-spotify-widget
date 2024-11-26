34894c1dd442a33c9849f852f5661a0f01aebe46
Rog3rc <ortizriosvictorguillermo@gmail.com>
2024-11-26 17:51:01 -0600

    msg: exclude config

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-  // URL: "https://obs-try-widget.vercel.app",
-  URL: "http://localhost:5500",
+  URL: "https://obs-try-widget.vercel.app",
+  // URL: "http://localhost:5500",
 };
